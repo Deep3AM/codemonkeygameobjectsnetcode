29345c9f4ab39b7b38a1a7ec287aef9440af18c0
deep3am <deep3am@gmail.com>
2024-03-05 17:00:12 +0900

    serverRpc

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,20 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData
+        {
+            _int = 56,
+            _bool = true,
+        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; randomNumber: " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+        };
+    }
     private void Update()
     {
         if (!IsOwner) return;
-        Vector3 moveDir =new Vector3(0,0,0);
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            //randomNumber.Value =
+            //    new MyCustomData
+            //    {
+            //        _int = 1,
+            //        _bool = false,
+            //        message = "All you base are belong to us!"
+            //    };
+            TestServerRpc(new ServerRpcParams());
+        }
+
+        Vector3 moveDir = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W)) moveDir.z += 1f;
         if (Input.GetKey(KeyCode.S)) moveDir.z += -1f;
         if (Input.GetKey(KeyCode.A)) moveDir.x += -1f;
         if (Input.GetKey(KeyCode.D)) moveDir.x += 1f;
         float moveSpeed = 2f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+    [ServerRpc]
+    private void TestServerRpc(ServerRpcParams serverRpcParams)
+    {
+        Debug.Log("TestServerRpc " + OwnerClientId + "; " + serverRpcParams.Receive.SenderClientId);
     }
 }
